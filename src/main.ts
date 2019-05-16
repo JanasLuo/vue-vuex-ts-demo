5a694c4b74290459096f5073ad0f41faa9910a40
janasluo <janas_luo@126.com>
2019-05-16 13:04:56 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './stores'
+import echarts from 'echarts'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.prototype = Object.assign(Vue.prototype, {
+  echarts
+})
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: (h) => h(App)
+}).$mount('#app')
